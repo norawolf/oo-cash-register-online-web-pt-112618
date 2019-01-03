b9baa659b39a31d58c71c2da75dbec0fc450f2d0
Nora Vogt <noraevogt@gmail.com>
2019-01-03 15:40:22 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -22,9 +22,10 @@ class CashRegister
 
   def apply_discount
     if @discount != 0
+      binding.pry
       @total = @total * (@discount.to_f/100)
       "After the discount, the total comes to $#{@total}."
-      binding.pry
+      
     else
       "There is no discount to apply."
     end
