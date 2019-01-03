0677d2349031c2d05b6bd838dde92f38dc84b7c8
Nora Vogt <noraevogt@gmail.com>
2019-01-03 14:51:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,13 @@
+class CashRegister
+  attr_accessor :total, :discount
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+  # def self.discount(disc)
+  #   self.new
+  # end
+
+end
