b07b5c0b323d78130da95887064050db51b8dfeb
Nora Vogt <noraevogt@gmail.com>
2019-01-03 15:09:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,17 +1,18 @@
 class CashRegister
-  attr_accessor :total, :discount, :price, :items
+  attr_accessor :total, :discount, :price
 
+  @@items = []
+  
   def initialize(discount = 0)
     @total = 0
     @discount = discount
-    @items = []
+    
   end
 
   def add_item(title, price, quantity = 1)
     @price = price
     @total += price * quantity
-    @items << title
-    
+    @@items << title
   end
 
 end
